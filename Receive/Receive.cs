fdff221fbc73d4620a5fcdc36c4157674112b6ab
Fernando <ferzim@gmail.com>
2020-05-31 16:29:03 -0300

    Adicionando aplicações console para consumir/produzir no RabbitMQ

---
+++ Receive/Receive.cs
@@ -0,0 +1,60 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Receive
+{
+    public class Receive
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5673,
+                UserName = "teste",
+                Password = "Teste2020!"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "queue-products",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        try
+                        {
+                            var body = ea.Body;
+                            var message = Encoding.UTF8.GetString(body.ToArray());
+
+                            Console.WriteLine($" A mensagem '{message}' foi recebida com sucesso.");
+
+                            channel.BasicAck(ea.DeliveryTag, false);
+                        }
+                        catch (Exception)
+                        {
+                            channel.BasicNack(ea.DeliveryTag, false, true);
+                        }
+                    };
+
+                    channel.BasicConsume(queue: "queue-products",
+                                         autoAck: false,
+                                         consumer: consumer);
+
+                    Console.WriteLine("Caso existam mensagens na fila, o console vai exibi-las,");
+                    Console.WriteLine("ou conforme novas mensagens forem entrando na fila.");
+                    Console.WriteLine("Caso queira fechar a aplicação, aperte [ENTER].");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    public class Send
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() 
+            {  
+                HostName = "localhost",
+                Port = 5673,
+                UserName = "teste",
+                Password = "Teste2020!"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "queue-products",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var close = "";
+                    Console.WriteLine(" Digite sua mensagem e aperte [ENTER] para enviá-la para a fila");
+                    Console.WriteLine(" ou digite 000 e aperte [ENTER] para sair.");
+
+                    do
+                    {
+                        Console.WriteLine("Mensagem: ");
+                        var message = Console.ReadLine();
+
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish(exchange: "exc-product",
+                                            routingKey: "queue-products",
+                                            basicProperties: null,
+                                            body: body);
+
+                        Console.WriteLine($"A mensagem {message} foi enviada para a fila do RabbitMQ.");
+
+                        Console.WriteLine(" Pressione [ENTER] para continuar enviando mensagens ou 000 para sair.");
+                        close = Console.ReadLine();
+                    } while (close != "000");
+                }
+            }
+
+            Console.WriteLine(" [ENTER] para encerrar a aplicação.");
+            Console.ReadLine();
+        }
+    }
+}
